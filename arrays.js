6daca595844fe4f94f4705badc26860c9d69e21c
Kate22009 <vorovskayak@gmail.com>
2023-11-27 15:55:29 +0200

    Adding arrays file

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let fruits = ['apple', 'orange', 'banana'];
+
+fruits.push('grape'); // Add an element to the end
+let poppedFruit = fruits.pop(); // Remove and return the last element
+
+console.log("Updated Array:", fruits); // ['apple', 'orange', 'banana', 'grape']
+console.log("Popped Fruit:", poppedFruit); // grape
